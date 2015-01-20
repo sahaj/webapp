c87ff68db64e5f16f730e2cd7bea811f817b5472
Sahaj Biyani <sahaj@169-231-137-154.wireless.ucsb.edu>
2015-01-20 11:57:00 -0800

    Home Page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,7 +4,7 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title", "Home | StockWIT"
+    assert_select "title", "StockWIT"
   end
 
   test "should get help" do
